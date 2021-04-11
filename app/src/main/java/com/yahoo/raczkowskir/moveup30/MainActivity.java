b9a356c8ffe9defddff895c49f1a494727dcefce
raczkowskir <raczkowskir@yahoo.com>
2021-04-11 19:59:53 +0200

    2. Done MOV-5

--- app/src/main/java/com/yahoo/raczkowskir/moveup30/MainActivity.java
+++ app/src/main/java/com/yahoo/raczkowskir/moveup30/MainActivity.java
@@ -19,68 +19,84 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editTextMin, editTextSec;
     Boolean isStopped;
-
+    boolean isFirstStart = true;
 
     public void protectValue(EditText editText, int valueOfTiemr) {
         String contentofEditTextSec = String.valueOf(editText.getText());
-        if (Integer.valueOf(contentofEditTextSec) > valueOfTiemr ) {
+        if ( contentofEditTextSec.length() > 2
+                || Integer.parseInt(contentofEditTextSec) > valueOfTiemr ) {
             editText.setText(String.valueOf(valueOfTiemr));
         }
+    }
+
+    public int getTimer(EditText aEditTextMin, EditText aEditTextSec, TextView aTextView) {
+        int editTextMin = Integer.parseInt(String.valueOf(aEditTextMin.getText()));
+        int editTextSec = Integer.parseInt(String.valueOf(aEditTextSec.getText()));
+        int fullTimer = (editTextMin*60) + editTextSec;
+        Log.d("Raki", "editTextMin " + editTextMin);
+        Log.d("Raki", "editTextSec " + editTextSec);
+        Log.d("Raki", "Full timer " + fullTimer);
+
+
+        counter = fullTimer;
+        counterStartValue = fullTimer;
+
+        if (isFirstStart) {
+            aTextView.setText(String.valueOf(fullTimer));
+            isFirstStart = false;
+        }
+
+        return fullTimer;
+    }
+
+    public void manageTimer(TextView aTextView) {
+        isStopped = false;
+
+        String currentTimer = String.valueOf(textView.getText());
+        counter = Integer.parseInt(currentTimer);
+
+        new CountDownTimer((counter*100), 100){
+            public void onTick(long millisUntilFinished){
+                if (!isStopped) {
+                    counter--;
+                    aTextView.setText(String.valueOf(counter));
+                }
+                else {
+                    this.cancel();
+                    counter = Integer.parseInt(String.valueOf(aTextView.getText()));
+                    Log.d("counter", String.valueOf(counter));
+                }
+            }
+            public  void onFinish(){
+                if (!isStopped) {
+                    aTextView.setText(String.valueOf(counterStartValue));
+                }
+                else {
+                    Log.d("Raki", "timerCanceled");
+                }
+            }
+        }.start();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonStart = (Button) findViewById(R.id.buttonStart);
-        buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
 
-        textView = (TextView) findViewById(R.id.textViewTimer);
-        editTextSec = (EditText) findViewById(R.id.editTextSec);
-        editTextMin = (EditText) findViewById(R.id.editTextMin);
+        textView = findViewById(R.id.textViewTimer);
+        editTextSec = findViewById(R.id.editTextSec);
+        editTextMin = findViewById(R.id.editTextMin);
 
-        buttonStart.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
+        buttonStart.setOnClickListener(v -> {
 
-                protectValue(editTextSec, 60);
-                protectValue(editTextMin, 99);
-
-                isStopped = false;
-                String currentTimer = String.valueOf(textView.getText());
-                counter = Integer.valueOf(currentTimer);
-
-                new CountDownTimer((counter*100), 100){
-                    public void onTick(long millisUntilFinished){
-                        if (isStopped == false) {
-                            counter--;
-                            textView.setText(String.valueOf(counter));
-                        }
-                        else {
-                            this.cancel();
-                            counter = Integer.valueOf(String.valueOf(textView.getText()));
-                            Log.d("counter", String.valueOf(counter));
-                        }
-                    }
-                    public  void onFinish(){
-                        if (isStopped == false) {
-                            textView.setText(String.valueOf(counterStartValue));
-                        }
-                        else {
-                            Log.d("Raki", "timerCanceled");
-                        }
-                    }
-                }.start();
-            }
+            protectValue(editTextSec, 60);
+            protectValue(editTextMin, 99);
+            getTimer(editTextMin, editTextSec, textView);
+            manageTimer(textView);
         });
 
-        buttonStop.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                isStopped = true;
-            }
-        });
+        buttonStop.setOnClickListener(v -> isStopped = true);
     }
 }
