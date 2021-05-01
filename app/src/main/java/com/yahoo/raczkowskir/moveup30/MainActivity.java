269981572afd5b1f475e74260c75e2476a145a9f
Raki <raczkowskir@yahoo.com>
2021-05-01 09:16:23 +0200

    Ongoing: MOV-11 (deleted comments, adjusted access modifiers, deleted some redundant code)

--- app/src/main/java/com/yahoo/raczkowskir/moveup30/MainActivity.java
+++ app/src/main/java/com/yahoo/raczkowskir/moveup30/MainActivity.java
@@ -1,51 +1,47 @@
 package com.yahoo.raczkowskir.moveup30;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    Button buttonStart, buttonStop, buttonClear;
-    TextView textView;
-    EditText editTextMin, editTextSec;
-    ConstraintLayout constraintLayout;
-    MainActivity mainActivity = this;
-    TimerUtilities timerUtilities;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-    MediaPlayer mp, mp_finish;
-    Context context = this;
+public class MainActivity extends AppCompatActivity {
+    private final MainActivity mMainActivity;
+    private final Context mContext;
+    TextView mTextView;
+    EditText mEditTextMin, mEditTextSec;
+    ConstraintLayout mConstraintLayout;
+    MediaPlayer mMp, mMp_finish;
+    private TimerUtilities mTimerUtilities;
+
+    public MainActivity() {
+        mMainActivity = this;
+        mContext = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        constraintLayout = findViewById(R.id.viewId);
-        buttonStart = findViewById(R.id.buttonStart);
-        buttonStop = findViewById(R.id.buttonStop);
-        buttonClear = findViewById(R.id.buttonClear);
-        textView = findViewById(R.id.textViewTimer);
-        editTextSec = findViewById(R.id.editTextSec);
-        editTextMin = findViewById(R.id.editTextMin);
+        mConstraintLayout = findViewById(R.id.viewId);
+        Button buttonStart = findViewById(R.id.buttonStart);
+        Button buttonStop = findViewById(R.id.buttonStop);
+        Button buttonClear = findViewById(R.id.buttonClear);
+        mTextView = findViewById(R.id.textViewTimer);
+        mEditTextSec = findViewById(R.id.editTextSec);
+        mEditTextMin = findViewById(R.id.editTextMin);
+        mMp = MediaPlayer.create(mContext, R.raw.alarm_clock);
+        mMp_finish = MediaPlayer.create(mContext, R.raw.alarm_clock_finish);
+        mTimerUtilities = new TimerUtilities(mContext, mMainActivity);
 
-        timerUtilities = new TimerUtilities(context, mainActivity);
-        mp = MediaPlayer.create(context, R.raw.alarm_clock);
-        mp_finish = MediaPlayer.create(context, R.raw.alarm_clock_finish);
-
-        buttonStart.setOnClickListener(v -> {
-            timerUtilities.handelButtonStart();
-        });
-
-        buttonStop.setOnClickListener(v -> timerUtilities.isStopped = true);
-
-        buttonClear.setOnClickListener(v -> {
-            timerUtilities.handelButtonClear();
-        });
+        buttonStart.setOnClickListener(v -> mTimerUtilities.handelButtonStart());
+        buttonStop.setOnClickListener(v -> mTimerUtilities.mIsStopped = true);
+        buttonClear.setOnClickListener(v -> mTimerUtilities.handelButtonClear());
     }
 }
--- app/src/main/java/com/yahoo/raczkowskir/moveup30/TimerUtilities.java
+++ app/src/main/java/com/yahoo/raczkowskir/moveup30/TimerUtilities.java
@@ -3,161 +3,146 @@ package com.yahoo.raczkowskir.moveup30;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.content.Context;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class TimerUtilities {
-
-    int counter = 100;
-    int preTimerCounter = 3;
-    int counterStartValue = 100;
-    boolean isFirstStart = true;
-    Boolean isStopped, isCleared;
-    ChainedCountDownTimer timer1;
-    ChainedCountDownTimer timer2;
-    Context context;
-    MainActivity mainActivity;
+    Boolean mIsStopped, mIsCleared;
+    private int mCounter = 100;
+    private int mPreTimerCounter = 3;
+    private int mCounterStartValue = 100;
+    private boolean mIsFirstStart = true;
+    private ChainedCountDownTimer mTimer1, mTimer2;
+    private Context mContext;
+    private MainActivity mMainActivity;
 
     public TimerUtilities(
             Context aContext,
             MainActivity aMainActivity
     ) {
-        this.context = aContext;
-        this.mainActivity = aMainActivity;
+        this.mContext = aContext;
+        this.mMainActivity = aMainActivity;
     }
 
-    public void protectValue(EditText aEditText, int aValueOfTiemr) {
+    private void protectValue(EditText aEditText, int aValueOfTiemr) {
         String contentofEditTextSec = String.valueOf(aEditText.getText());
         if (contentofEditTextSec.length() > 2
                 || Integer.parseInt(contentofEditTextSec) > aValueOfTiemr) {
             aEditText.setText(String.valueOf(aValueOfTiemr));
         }
-        Log.d("Raki_3", String.valueOf("protectValue"));
     }
 
-    public int getTimer(EditText aEditTextMin, EditText aEditTextSec, TextView aTextView) {
-        Log.d("Raki_4", String.valueOf("getTimer"));
+    private int getTimer(EditText aEditTextMin, EditText aEditTextSec, TextView aTextView) {
         int editTextMin = Integer.parseInt(String.valueOf(aEditTextMin.getText()));
         int editTextSec = Integer.parseInt(String.valueOf(aEditTextSec.getText()));
         int fullTimer = (editTextMin * 60) + editTextSec;
-        Log.d("Raki", "editTextMin " + editTextMin);
-        Log.d("Raki", "editTextSec " + editTextSec);
-        Log.d("Raki", "Full timer " + fullTimer);
 
+        mCounter = fullTimer;
+        mCounterStartValue = fullTimer;
 
-        counter = fullTimer;
-        counterStartValue = fullTimer;
-
-        if (isFirstStart) {
+        if (mIsFirstStart) {
             aTextView.setText(String.valueOf(fullTimer));
-            isFirstStart = false;
+            mIsFirstStart = false;
         }
-
         return fullTimer;
     }
 
-    public Boolean startPreTimer(TextView aTextView) {
-        timer1 = new ChainedCountDownTimer((4000), 1000) {
+    private Boolean startPreTimer(TextView aTextView) {
+        mTimer1 = new ChainedCountDownTimer((4000), 1000) {
 
             public void onTick(long millisUntilFinished) {
                 try {
-                    if (mainActivity.mp.isPlaying()) {
-                        mainActivity.mp.stop();
-                        mainActivity.mp.release();
-                        mainActivity.mp = MediaPlayer.create(context, R.raw.alarm_clock);
+                    if (mMainActivity.mMp.isPlaying()) {
+                        mMainActivity.mMp.stop();
+                        mMainActivity.mMp.release();
+                        mMainActivity.mMp = MediaPlayer.create(mContext, R.raw.alarm_clock);
                     }
-                    mainActivity.mp.start();
+                    mMainActivity.mMp.start();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-                aTextView.setText(String.valueOf(preTimerCounter));
-                preTimerCounter--;
-                Log.d("Raki_1", String.valueOf(preTimerCounter));
+                aTextView.setText(String.valueOf(mPreTimerCounter));
+                mPreTimerCounter--;
             }
 
             public void onFinish() {
 
                 try {
-                    if (mainActivity.mp_finish.isPlaying()) {
-                        mainActivity.mp_finish.stop();
-                        mainActivity.mp_finish.release();
-                        mainActivity.mp_finish = MediaPlayer.create(context, R.raw.alarm_clock_finish);
+                    if (mMainActivity.mMp_finish.isPlaying()) {
+                        mMainActivity.mMp_finish.stop();
+                        mMainActivity.mMp_finish.release();
+                        mMainActivity.mMp_finish = MediaPlayer.create(mContext, R.raw.alarm_clock_finish);
                     }
-                    mainActivity.mp_finish.start();
+                    mMainActivity.mMp_finish.start();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-                mainActivity.constraintLayout.setBackgroundColor(Color.rgb(76, 175, 80));
-                Log.d("Raki_2", String.valueOf(preTimerCounter));
+                mMainActivity.mConstraintLayout.setBackgroundColor(Color.rgb(76, 175, 80));
                 aTextView.setText(String.valueOf(0));
-                preTimerCounter = 3;
+                mPreTimerCounter = 3;
             }
         };
         return true;
     }
 
-    public void manageTimer(TextView aTextView) {
-        Log.d("Raki_5", String.valueOf("manageTimer"));
-        isStopped = false;
-        isCleared = false;
+    private void manageTimer(TextView aTextView) {
+        mIsStopped = false;
+        mIsCleared = false;
 
-        String currentTimer = String.valueOf(mainActivity.textView.getText());
-        counter = Integer.parseInt(currentTimer);
+        String currentTimer = String.valueOf(mMainActivity.mTextView.getText());
+        mCounter = Integer.parseInt(currentTimer);
 
-        timer2 = new ChainedCountDownTimer((counter * 100), 100) {
+        mTimer2 = new ChainedCountDownTimer((mCounter * 100), 100) {
             public void onTick(long millisUntilFinished) {
-                Log.d("Raki_7", String.valueOf(counter));
-                if (!isStopped) {
-                    counter--;
-                    aTextView.setText(String.valueOf(counter));
+                if (!mIsStopped) {
+                    mCounter--;
+                    aTextView.setText(String.valueOf(mCounter));
                 } else {
                     this.cancel();
-                    if (!isCleared) {
-                        counter = Integer.parseInt(String.valueOf(aTextView.getText()));
-                        Log.d("counter", String.valueOf(counter));
+                    if (!mIsCleared) {
+                        mCounter = Integer.parseInt(String.valueOf(aTextView.getText()));
                     } else {
-                        counter = Integer.parseInt(String.valueOf(0));
-                        Log.d("Raki", "Cleared");
+                        mCounter = Integer.parseInt(String.valueOf(0));
                     }
                 }
             }
 
             public void onFinish() {
-                if (!isStopped) {
-                    aTextView.setText(String.valueOf(counterStartValue));
-                    mainActivity.constraintLayout.setBackgroundColor(Color.rgb(255, 255, 255));
+                if (!mIsStopped) {
+                    aTextView.setText(String.valueOf(mCounterStartValue));
+                    mMainActivity.mConstraintLayout.setBackgroundColor(Color.rgb(
+                            255,
+                            255,
+                            255));
                 } else {
-                    mainActivity.constraintLayout.setBackgroundColor(Color.rgb(255, 255, 255));
-                    Log.d("Raki", "timerCanceled");
+                    mMainActivity.mConstraintLayout.setBackgroundColor(Color.rgb(
+                            255,
+                            255,
+                            255));
                 }
             }
         };
     }
 
     public void handelButtonStart() {
-        startPreTimer(mainActivity.textView);
-        protectValue(mainActivity.editTextSec, 60);
-        protectValue(mainActivity.editTextMin, 99);
-        getTimer(mainActivity.editTextMin, mainActivity.editTextSec, mainActivity.textView);
-        manageTimer(mainActivity.textView);
-        Log.d("Raki_6", String.valueOf("onCreate"));
-        mainActivity.constraintLayout.setBackgroundColor(Color.rgb(202, 32, 32));
-        timer1.setNext(timer2).start();
+        startPreTimer(mMainActivity.mTextView);
+        protectValue(mMainActivity.mEditTextSec, 60);
+        protectValue(mMainActivity.mEditTextMin, 99);
+        getTimer(mMainActivity.mEditTextMin, mMainActivity.mEditTextSec, mMainActivity.mTextView);
+        manageTimer(mMainActivity.mTextView);
+        mMainActivity.mConstraintLayout.setBackgroundColor(Color.rgb(202, 32, 32));
+        mTimer1.setNext(mTimer2).start();
     }
 
     public void handelButtonClear() {
-        mainActivity.constraintLayout.setBackgroundColor(Color.rgb(255, 255, 255));
-        isFirstStart = true;
-        protectValue(mainActivity.editTextSec, 60);
-        protectValue(mainActivity.editTextMin, 99);
-        getTimer(mainActivity.editTextMin, mainActivity.editTextSec, mainActivity.textView);
-        isStopped = true;
-        isCleared = true;
+        mMainActivity.mConstraintLayout.setBackgroundColor(Color.rgb(255, 255, 255));
+        mIsFirstStart = true;
+        protectValue(mMainActivity.mEditTextSec, 60);
+        protectValue(mMainActivity.mEditTextMin, 99);
+        getTimer(mMainActivity.mEditTextMin, mMainActivity.mEditTextSec, mMainActivity.mTextView);
+        mIsStopped = true;
+        mIsCleared = true;
     }
 }
